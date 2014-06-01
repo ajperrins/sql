1f897953412ae0a993ecf89fbe0e564d94977801
unknown <andrew.perrins@gmail.com>
2014-06-01 13:36:41 -0400

    Including running transactions query

---
+++ scripts/Running Transactions.sql
@@ -0,0 +1,12 @@
+SELECT sqltext.TEXT,
+req.session_id,
+req.status,
+req.command,
+req.cpu_time,
+req.total_elapsed_time
+FROM sys.dm_exec_requests req
+CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext
+
+--kill 77
+
+UPDATE [Admiral_Database].[dbo].[tblOrderSpot] SET [FlagIsFiller] = DEFAULT
